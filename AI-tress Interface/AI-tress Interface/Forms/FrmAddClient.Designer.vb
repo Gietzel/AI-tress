749f0032c4862466232ed1508fd90ab09c6f73c1
Gabriel Gietzel <gabrielgietzelalemao@gmail.com>
2025-07-01 09:16:15 -0300

    Adding error icon

--- AI-tress Interface/AI-tress Interface/Forms/FrmAddClient.Designer.vb
+++ AI-tress Interface/AI-tress Interface/Forms/FrmAddClient.Designer.vb
@@ -25,11 +25,11 @@ Partial Class FrmAddClient
         components = New ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmAddClient))
         BtnClose = New Button()
-        BtnAddClient = New Button()
         ImageList32 = New ImageList(components)
+        BtnAddClient = New Button()
         MskClientNum = New MaskedTextBox()
         LblTitulo = New Label()
-        Label2 = New Label()
+        LblResponse = New Label()
         PicIcon = New PictureBox()
         Label1 = New Label()
         ToolTip1 = New ToolTip(components)
@@ -53,6 +53,14 @@ Partial Class FrmAddClient
         ToolTip1.SetToolTip(BtnClose, "Fechar tela")
         BtnClose.UseVisualStyleBackColor = True
         ' 
+        ' ImageList32
+        ' 
+        ImageList32.ColorDepth = ColorDepth.Depth32Bit
+        ImageList32.ImageStream = CType(resources.GetObject("ImageList32.ImageStream"), ImageListStreamer)
+        ImageList32.TransparentColor = Color.Transparent
+        ImageList32.Images.SetKeyName(0, "004-add.png")
+        ImageList32.Images.SetKeyName(1, "034-cancel.png")
+        ' 
         ' BtnAddClient
         ' 
         BtnAddClient.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
@@ -70,14 +78,6 @@ Partial Class FrmAddClient
         ToolTip1.SetToolTip(BtnAddClient, "Cadastrar cliente")
         BtnAddClient.UseVisualStyleBackColor = False
         ' 
-        ' ImageList32
-        ' 
-        ImageList32.ColorDepth = ColorDepth.Depth32Bit
-        ImageList32.ImageStream = CType(resources.GetObject("ImageList32.ImageStream"), ImageListStreamer)
-        ImageList32.TransparentColor = Color.Transparent
-        ImageList32.Images.SetKeyName(0, "004-add.png")
-        ImageList32.Images.SetKeyName(1, "034-cancel.png")
-        ' 
         ' MskClientNum
         ' 
         MskClientNum.BorderStyle = BorderStyle.FixedSingle
@@ -92,33 +92,33 @@ Partial Class FrmAddClient
         ' 
         LblTitulo.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
         LblTitulo.AutoSize = True
-        LblTitulo.Font = New Font("Rockwell", 21.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LblTitulo.Font = New Font("Microsoft Sans Serif", 21.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         LblTitulo.ForeColor = Color.FromArgb(CByte(235), CByte(42), CByte(83))
         LblTitulo.Location = New Point(275, 17)
         LblTitulo.Margin = New Padding(4, 0, 4, 0)
         LblTitulo.Name = "LblTitulo"
-        LblTitulo.Size = New Size(270, 36)
+        LblTitulo.Size = New Size(259, 33)
         LblTitulo.TabIndex = 30
         LblTitulo.Text = "Cadastrar Cliente"
         ' 
-        ' Label2
+        ' LblResponse
         ' 
-        Label2.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
-        Label2.Font = New Font("Modern No. 20", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label2.ForeColor = Color.Crimson
-        Label2.Location = New Point(163, 336)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(510, 29)
-        Label2.TabIndex = 31
-        Label2.Text = "Cliente adicionado com sucesso!"
-        Label2.TextAlign = ContentAlignment.MiddleCenter
-        Label2.Visible = False
+        LblResponse.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        LblResponse.Font = New Font("Microsoft Sans Serif", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LblResponse.ForeColor = Color.Crimson
+        LblResponse.Location = New Point(282, 336)
+        LblResponse.Name = "LblResponse"
+        LblResponse.Size = New Size(351, 29)
+        LblResponse.TabIndex = 31
+        LblResponse.Text = "Cliente adicionado com sucesso!"
+        LblResponse.TextAlign = ContentAlignment.MiddleLeft
+        LblResponse.Visible = False
         ' 
         ' PicIcon
         ' 
         PicIcon.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
         PicIcon.Image = CType(resources.GetObject("PicIcon.Image"), Image)
-        PicIcon.Location = New Point(272, 336)
+        PicIcon.Location = New Point(252, 336)
         PicIcon.Name = "PicIcon"
         PicIcon.Size = New Size(24, 29)
         PicIcon.SizeMode = PictureBoxSizeMode.Zoom
@@ -129,11 +129,11 @@ Partial Class FrmAddClient
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Font = New Font("Modern No. 20", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Font = New Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label1.ForeColor = Color.Crimson
         Label1.Location = New Point(327, 171)
         Label1.Name = "Label1"
-        Label1.Size = New Size(156, 15)
+        Label1.Size = New Size(174, 16)
         Label1.TabIndex = 33
         Label1.Text = "Digite o número de telefone:"
         ' 
@@ -145,7 +145,7 @@ Partial Class FrmAddClient
         ClientSize = New Size(798, 497)
         Controls.Add(Label1)
         Controls.Add(PicIcon)
-        Controls.Add(Label2)
+        Controls.Add(LblResponse)
         Controls.Add(LblTitulo)
         Controls.Add(MskClientNum)
         Controls.Add(BtnAddClient)
@@ -161,7 +161,7 @@ Partial Class FrmAddClient
     Private WithEvents BtnAddClient As Button
     Friend WithEvents MskClientNum As MaskedTextBox
     Private WithEvents LblTitulo As Label
-    Friend WithEvents Label2 As Label
+    Friend WithEvents LblResponse As Label
     Friend WithEvents PicIcon As PictureBox
     Friend WithEvents Label1 As Label
     Friend WithEvents ImageList32 As ImageList
--- AI-tress Interface/AI-tress Interface/Forms/FrmAddClient.vb
+++ AI-tress Interface/AI-tress Interface/Forms/FrmAddClient.vb
@@ -1,4 +1,28 @@
-﻿Public Class FrmAddClient
+﻿Imports System.Text.RegularExpressions
+
+Public Class FrmAddClient
+
+#Region "Variables"
+
+    Private Property MSG_SUCCESS_INSERT = "Cliente adicionado com sucesso!"
+    Private Property MSG_ERROR_INSERT = "Erro durante inserção do cliente: "
+
+#End Region
+
+#Region "Main Methods"
+
+    Private Sub ShowDbResponse(isSuccess As Boolean)
+        If isSuccess Then
+            LblResponse.Text = MSG_SUCCESS_INSERT
+        Else
+            LblResponse.Text = MSG_ERROR_INSERT
+        End If
+
+        PicIcon.Visible = True
+        LblResponse.Visible = True
+    End Sub
+
+#End Region
 
 #Region "Events"
 
@@ -7,6 +31,28 @@
     End Sub
 
     Private Sub BtnAddClient_Click(sender As Object, e As EventArgs) Handles BtnAddClient.Click
+        Try
+            Dim stringClientNum As String = MskClientNum.Text
+            Dim clientNumConverted As String = Regex.Replace(stringClientNum, "[^\d]", "")
+            Dim clientNum As Long
+
+            If Long.TryParse(clientNumConverted, clientNum) Then
+                Dim newClient As New Clients With {
+                    .Number = clientNum
+                }
+
+                Using ctx = DbContextFactory.CreateContext()
+                    ctx.Clients.Add(newClient)
+                    ctx.SaveChanges()
+                End Using
+
+                ShowDbResponse(True)
+            Else
+                MessageBox.Show("Número de cliente inválido. Por favor, insira um número válido.")
+            End If
+        Catch ex As Exception
+            ShowDbResponse(False)
+        End Try
     End Sub
 
 #End Region
